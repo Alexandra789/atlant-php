6c5464c643d34c1c8112442ef436de350dafb7c4
Alexandra789 <zakovinko99@gmail.com>
2023-11-05 20:44:19 +0300

    fix validate form functions for many forms

--- src/js/validate.js
+++ src/js/validate.js
@@ -1,55 +1,44 @@
-const invoice = document.querySelector('#invoice');
-const typeDoc = document.querySelector('#typeDoc');
-const addRecordBtn = document.querySelector('.add-record-btn');
-const formBtn = document.querySelector('.add-record-btn');
-const form = document.querySelector('.form-add-record');
+const invoiceInputs = document.querySelectorAll('.form-control-invoice');
+const typeDocInputs = document.querySelectorAll('.form-control-typedoc');
+const addRecordBtn = document.querySelectorAll('.validate-btn');
 
 let errorArray = {};
 
-invoice.oninput = function (e) {
-    checkIsNumValue(e);
-};
+invoiceInputs.forEach(invoice => {
+    invoice.oninput = function (e) {
+        console.log(e);
+        checkIsNumValue(e);
+    };
+})
 
-typeDoc.oninput = function (e) {
-    checkIsNumValue(e);
-};
+typeDocInputs.forEach(typeDoc => {
+    typeDoc.oninput = function (e) {
+        checkIsNumValue(e);
+    };
+})
 
 function checkIsNumValue(e) {
     const helperText = e.target.parentElement.querySelector('.invalid-feedback');
+    const btn = e.target.parentElement.parentElement.parentElement.querySelector('.validate-btn');
     if (!isNaN(e.target.value)) {
         delete errorArray[e.target.name];
-        removeError(helperText);
+        removeError(helperText, btn);
     } else {
         errorArray[e.target.name] = helperText.innerText;
-        addError(helperText);
+        addError(helperText, btn);
     }
 
-    function addError(helperText) {
+    function addError(helperText, btn) {
         helperText.classList.remove('d-none');
-        addRecordBtn.classList.add('disabled');
+        btn.classList.add('disabled');
     }
 
-    function removeError(helperText) {
+    function removeError(helperText, btn) {
         helperText.classList.add('d-none');
-        console.log(Object.keys(errorArray).length)
+        console.log(Object.keys(errorArray).length);
 
         if (Object.keys(errorArray).length === 0) {
-            addRecordBtn.classList.remove('disabled');
+            btn.classList.remove('disabled');
         }
     }
 }
-
-// form.addEventListener('submit',(e)=>{
-//
-//
-// })
-
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     setTimeout(e.stopPropagation,5000)
-//     console.log(e)
-//     if (Object.keys(errorArray).length === 0) {
-//         form.submit();
-//         form.reset();
-//     }
-// })
